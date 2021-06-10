ea6d1aa1fd158471a3b22512aef8ab5c514d3529
CemilUysal <16050111006@ybu.edu.tr>
2021-06-10 21:43:11 +0300

    Project

--- sprint3/timeline_page/timeline.js
+++ sprint3/timeline_page/timeline.js
@@ -12,8 +12,8 @@ $(document).ready(function(){
     function renderHtml(data){
         for(i = 0 ; i<data.length; i++){
            commentsHtml += '<div class="card my-5 "><div class="card-body">'+
-           '<img src="https://via.placeholder.com/150x150" alt="image" class="personal-images" height="200px"/>'+
-           '<div class=" card comment w-75 float-end"> <div class="card-header">'+
+           '<img src="https://via.placeholder.com/150x150" alt="image" class="personal-images image'+i+ '"height="200px"'+
+           '/><div class=" card comment w-75 float-end"> <div class="card-header">'+
            '<h3 class="card-title "><a href="">' + data[i].user +'</a></h3></div>'+
            '<div class="card-body"><h4 class="card-title"><a href="">'+data[i].name +
            '</a></h4></div><div class="card-footer"><p class="card-text ">'+ data[i].comments+
@@ -21,8 +21,8 @@ $(document).ready(function(){
            
            $(".container-timeline").html(commentsHtml);
         }
-        for(i = 0; i<data.length; i++){
-            $(".personal-images").attr("src", data[i].image);
+        for(i = 0; i<data.length;i++){
+            $(".image"+i).attr("src",data[i].image);
         }
     }
    
