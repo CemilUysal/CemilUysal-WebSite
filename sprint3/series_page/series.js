1661d2e72bd33f337f7c422996e17e6361787a1f
CemilUysal <16050111006@ybu.edu.tr>
2021-06-10 16:44:32 +0300

    Project

--- sprint3/series_page/series.js
+++ sprint3/series_page/series.js
@@ -1,29 +1,7 @@
 $(function(){
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        nav:true,
-        margin:10,
-        responsiveClass:true,
-        lazyLoad: true,
-        video: true,
-        responsive:{
-            0:{
-                items:1,
-                nav:true
-            },
-            600:{
-                items:3,
-                nav:true
-            },
-            900:{
-                items:5,
-                nav:true,
-                loop:true
-            }
-        }
-    });
-    
-    $(".card-footer").mouseover(function(){
+    var comments="";
+    $(".series").mouseover(function(){
+        var cardId = "";
         var cardId = "#"+$(this).attr("id");
         $(cardId + " #s5").click(function(){
             $(cardId + " .fa-star").css("color", "white");
@@ -48,15 +26,42 @@ $(function(){
             $( cardId+" #s1").css("color", "red");
             $( cardId+" #s2").css("color", "red");
         });
-        $(cardId + " #s4").click(function(){
+        $(cardId + " .series-footer #s4").click(function(){
             $(cardId + " .fa-star").css("color", "white");
             $(this).css("color","red");
             $( cardId+" #s1").css("color", "red");
             $( cardId+" #s2").css("color", "red");
             $( cardId+" #s3").css("color", "red");
+            
         });
+        
+        $(cardId + " .series-footer .fa-star").click(function(){
+            $(cardId+ " .comments").val(comments);
+            $(cardId+" .comments").show("slide",500);
+            
+        });
+        
         
     });
     
-
+    $(".series").mouseover(function(){
+        var cardId = "";
+        
+        cardId = "#"+$(this).attr("id");
+        $(".comments").mouseover(function(){
+            
+            commentsId= "#"+$(this).attr("id");
+            $(".comments-button").click(function(){
+                
+                comments = $(cardId+ " .comments").val();
+                $(cardId+" " +commentsId).hide("fold",500);
+            });
+        });
+       
+        
+        
+    });
+    
+    
+    
 });
