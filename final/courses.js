49c659897f69b0b12f4bd16e68ebde8d127869fc
CemilUysal <16050111006@ybu.edu.tr>
2021-06-21 15:15:32 +0300

    exam

--- final/courses.js
+++ final/courses.js
@@ -1,39 +1,25 @@
-$(function(){   
-    var code="";
-    // every list button have class="lesson"
-    
-    $(".lesson").click(function(){
-        //they keep own class code for value ex. value="CENG311"
-        code = $(this).val();
-        
-            var ourRequest = new XMLHttpRequest();
-            
-            ourRequest.open("GET","courses.json");
-            ourRequest.onload = function(){
-                var ourData = JSON.parse(ourRequest.responseText);
-                renderHtml(ourData);
-            };
-            ourRequest.send(); 
+$(document).ready(function() {
+
+	var cities = ["ankara", "izmir", "istanbul", "konya", "edirne", "samsun", "bursa", "trabzon"]; //Collection of words 
+
+	var selection = Math.random()*cities.length;  	// Selecting the target word from the collection
+
+	var word = cities[Math.floor(selection)]; 		// Casting to an int value
+
+	var lives = 5; 									// Number of tries we have
+
+	$('#mylives').html("You have " + lives + " lives left"); 
+
+	for(var i = 0; i<word.length;i++) // Displaying _ character for each unknown letter in the target word
+
+	{
+
+		$('#theword').append("_ ");
+
+	}
+    $("#guess").click(function(){
+        alert("tıkladın");
     });
-    function renderHtml(data){
-        for(i = 0; i<data.length;i++){
-            var desc = ""
-            if(data[i].code == code){
-                desc += "Description: "
-                $(".code").text(data[i].code);
-                $(".name").text(data[i].name);
-                $(".lecturer").text("Lexturer: "+data[i].lecturer);
-                $(".term").text("Term: "+data[i].term);
-                $(".meeting").text("Meeting Time: "+data[i].meeting_Time);
-                $(".expected").text("Expected Grade: "+data[i].expected_Grade);
-                for(i = 0; i<data[i].description.length;i++){
-                    desc +=+data[i].description[i]+", ";
-                    
-                }
-                desc+="was told";
-                $(".description").text(desc);
-            }
-        }
-    }
 
 });
+
